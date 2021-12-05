9202735e44203690027af5e54fa3b4db230a8300
Linus Nwankwo <nwankwolinus9@gmail.com>
2021-12-05 20:50:00 +0100

    added image components

--- democomponent/src/app/app.module.ts
+++ democomponent/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextcomponentComponent
+    TextcomponentComponent,
+    ImagecomponentComponent
   ],
   imports: [
     BrowserModule,
